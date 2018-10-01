bed3f3df5bba6c2d71f2fbf1eb52e1033fc4a431
ebis19 <elu1996biscaia@gmail.com>
2018-09-30 22:20:53 -0300

    se saco codigo inecesario

--- src/conflitoentrereinas/Tablero.java
+++ src/conflitoentrereinas/Tablero.java
@@ -27,21 +27,14 @@ public class Tablero {
 		in.close();
 		
 	}
-	
 	public void resolver(){
+		int rec[][]= {{1,-1},{-1,-1},{0,-1},{1,0},{-1,1},{-1,-1},{1,1},{0,1}};
 		for (Reina reina : reinas) {
-			reina.agregarConflicto(buscarConflicto(reina,1,-1)); // izq ds
-			reina.agregarConflicto(buscarConflicto(reina,-1,-1));// izq dp
-			reina.agregarConflicto(buscarConflicto(reina,0,-1)); // izq h
-			reina.agregarConflicto(buscarConflicto(reina,1,0)); // arriva
-			reina.agregarConflicto(buscarConflicto(reina,-1,0)); //abajo
-			reina.agregarConflicto(buscarConflicto(reina,-1,1)); // der ds
-			reina.agregarConflicto(buscarConflicto(reina,1,1)); // der dp
-			reina.agregarConflicto(buscarConflicto(reina,0,1)); // der h
-			
+			for (int[] is : rec) {
+				reina.agregarConflicto(buscarConflicto(reina,is));
+			}
 		}		
 	}
-
 	public void guardarEnArchivo(String o) throws IOException{
 		
 		PrintWriter out = new PrintWriter(new File(o+".out"));
@@ -49,7 +42,33 @@ public class Tablero {
 			out.print(reina);
 		out.close();
 	}
-
+	private boolean estaEnTablero(int f , int c){
+		return f<tamano && c<tamano && f>=0 && c>=0;
+	}
+	private int buscarConflicto(Reina reina,int is[]) {
+		int posf=reina.f+is[0];
+		int posc=reina.c+is[1];
+		while(estaEnTablero(posf,posc)){
+			if(tablero[posf][posc]!=0)
+				return tablero[posf][posc];
+			posf+=is[0];
+			posc+=+is[1];
+			
+		}
+		return 0;
+	}
+}
+/*
+ * 
+ * 			reina.agregarConflicto(buscarConflicto(reina,1,-1)); // izq ds
+			reina.agregarConflicto(buscarConflicto(reina,-1,-1));// izq dp
+			reina.agregarConflicto(buscarConflicto(reina,0,-1)); // izq h
+			reina.agregarConflicto(buscarConflicto(reina,1,0)); // arriva
+			reina.agregarConflicto(buscarConflicto(reina,-1,0)); //abajo
+			reina.agregarConflicto(buscarConflicto(reina,-1,1)); // der ds
+			reina.agregarConflicto(buscarConflicto(reina,1,1)); // der dp
+			reina.agregarConflicto(buscarConflicto(reina,0,1)); // der h
+ * 
 	private int buscarConflictosDer(Reina reina) {
 		for (int i = reina.c; i < tamano; i++) {
 			if(tablero[reina.f][i]!=0)
@@ -57,24 +76,6 @@ public class Tablero {
 		}
 		return 0;
 	}
-	private boolean estaEnTablero(int f , int c){
-		return f<tamano && c<tamano && f>=0 && c>=0;
-	}
-	
-	private int buscarConflicto(Reina reina,int f,int c) {
-		int posf=reina.f+f;
-		int posc=reina.c+c;
-		while(estaEnTablero(posf,posc)){
-			if(tablero[posf][posc]!=0)
-				return tablero[posf][posc];
-			posf+=f;
-			posc+=c;
-			
-		}
-		return 0;
-	}
-	
-/*
 	private int buscarConflictosIzq(Reina reina) {
 			for (int i = reina.c; i<0; i--) {
 				if(tablero[reina.f][i]!=0)
@@ -144,4 +145,3 @@ public class Tablero {
 	
 	
 
-}
